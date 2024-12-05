d898a9ef49c6abd1551bcf91730c4a8d6638b9bb
Andry Ariadi <andryariadi23@gmail.com>
2024-12-05 16:19:03 +0700

    setup convex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import { ConvexClientProvider } from "./Providers/ConvexProvider";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${manrope.className} antialiased`}>{children}</body>
+      <body className={`${manrope.className} antialiased`}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
   );
 }
